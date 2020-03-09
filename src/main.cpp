d659cbc04947503b6ad5ada46d1ef8e9205e6ba8
Niglou <galaxneil.super@gmail.com>
2020-03-09 04:44:44 +0100

    Add main

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <ESP32/include/init.h>
+#include <ESP32/include/rom/ets_sys.h>
+
+extern "C" {
+  void main_cpu0();
+}
+
+void main_cpu0() {
+
+  /*
+    Disable Watchdog
+    Set PLL clock at 80Mhz
+    Move exceptions vectors to IRAM
+    Set UART div to 115200 bauds
+  */
+  init_esp32();
+  ets_install_uart_printf();
+  ets_update_cpu_frequency_rom(80);
+
+  /* ***** SETUP ***** */
+
+  /* Code */
+
+  /* ***** LOOP ***** */
+  while(1) {
+
+    ets_printf("Hello, World !\n");
+
+    ets_delay_us(1000000);
+
+  }
+
+
+}
